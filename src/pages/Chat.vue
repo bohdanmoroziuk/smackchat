f2af59a99dba364b170fa7cc5e345f72cafa1336
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-04-16 15:17:57 +0300

    feat(pages.chat): add message input

--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,11 +1,58 @@
 <template>
   <q-page class="flex">
-    Chat
+    <q-footer elevated>
+      <q-toolbar>
+        <q-input
+          class="full-width"
+          bg-color="white"
+          rounded
+          outlined
+          v-model.trim="message"
+          label="Message"
+          dense
+        >
+
+          <template v-slot:append>
+            <q-icon
+              v-if="hasMessage"
+              class="cursor-pointer"
+              name="close"
+              @click="clearMessage"
+            />
+          </template>
+
+          <template v-slot:after>
+            <q-btn
+              round
+              dense
+              flat
+              color="white"
+              icon="send"
+            />
+          </template>
+        </q-input>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'Chat',
+  data() {
+    return {
+      message: '',
+    };
+  },
+  computed: {
+    hasMessage() {
+      return this.message !== '';
+    },
+  },
+  methods: {
+    clearMessage() {
+      this.message = '';
+    },
+  },
 };
 </script>
