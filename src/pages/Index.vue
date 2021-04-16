228b76e5860cf540cf355cdcd3d94cf1e0a90602
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-04-16 14:58:00 +0300

    feat(pages.index): display list of users

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,67 @@
 <template>
-  <q-page class="flex">
-    Home
+  <q-page class="flex q-pa-sm">
+    <q-list
+      class="full-width"
+      separator
+    >
+      <q-item
+        v-for="user of users"
+        :key="user.id"
+        clickable
+        v-ripple
+        to="/chat"
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name | letter }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online | status }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'Index',
+  data() {
+    return {
+      users: [{
+        id: 1,
+        name: 'Ruddy',
+        online: false,
+      }, {
+        id: 2,
+        name: 'Mallorie',
+        online: false,
+      }, {
+        id: 3,
+        name: 'Elisabetta',
+        online: true,
+      }, {
+        id: 4,
+        name: 'Seka',
+        online: false,
+      }],
+    };
+  },
+  filters: {
+    letter(name) {
+      return name.charAt(0);
+    },
+    status(online) {
+      return online ? 'Online' : 'Offline';
+    },
+  },
 };
 </script>
